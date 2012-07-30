862297718ece9c5503b7ef231bb35fd6079dc631
Andrey Chernih <andrey.chernih@gmail.com>
2012-07-30 22:57:56 +0400

    vim is needed

--- cookbooks/bootstrap/recipes/default.rb
+++ cookbooks/bootstrap/recipes/default.rb
@@ -1,5 +1,4 @@
-package "build-essential"
-package "libshadow-ruby1.8"
+["build-essential", "vim", "libshadow-ruby1.8"].map{|p| package p}
 
 user "root" do
   password "$6$fixpt5OK6sKDzI1S$06ZJGbxjOhgdFTLcz.v.uY704pMvxloyUTtPHxGoguklWV..OF1l7XH2lNZUFKSZXdUdqgYWVABbrUS/mMtbZ1" # 12345
