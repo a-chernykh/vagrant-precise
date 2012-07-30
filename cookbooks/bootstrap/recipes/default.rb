d332b51fd6453d2775a405ea6c7741e908004b2f
Andrey Chernih <andrey.chernih@gmail.com>
2012-07-30 12:13:21 +0400

    Initial commit with Vagrantfile and bootstrap cookbook

---
+++ cookbooks/bootstrap/recipes/default.rb
@@ -0,0 +1,6 @@
+package "build-essential"
+package "libshadow-ruby1.8"
+
+user "root" do
+  password "$6$fixpt5OK6sKDzI1S$06ZJGbxjOhgdFTLcz.v.uY704pMvxloyUTtPHxGoguklWV..OF1l7XH2lNZUFKSZXdUdqgYWVABbrUS/mMtbZ1" # 12345
+end
